a062539de1578eaeffaec76f1fda9380caa13f10
Jake Rubin <jakerubin@ufl.edu>
2022-11-02 21:04:42 -0400

    deployer class v1

--- indexReactFmt.mjs
+++ indexReactFmt.mjs
@@ -73,3 +73,22 @@ class Player extends React.Component{
   playHand(hand) { this.state.resolveHandP(hand); }
   predict(guess) { this.setState.resolveGuessP(guess); }
 }
+
+class Deployer extends Player{
+  constructor(props){
+    super(props);
+    this.state = {view: 'SetWager'};
+  }
+  setWager(wager) { this.setState({view: 'Deploy', wager}); }
+  async deploy() {
+    const ctc = this.props.acc.contract(backend);
+    this.setState({view: 'Deploying', ctc});
+    this.wager = reach.parseCurrency(this.state.wager);
+    this.deadline = {ETH: 10, ALGO: 100, CFX: 1000}[reach.connector]; //change eth deadline for L2s
+    backend.Alice(ctc, this);
+    const ctcInfoStr = JSON.stringify(await ctc.getInfo(), null, 2);
+    this.setState({view: 'WaitingForAttacher', ctcInfoStr});
+  }
+  render () { return renderView(this, DeployerViews); }
+}
+
